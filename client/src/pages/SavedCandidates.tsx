1e76e338a9b88bef13e2dcb3c12df5fe95aebc61
Yahya Mohamed <yahyemohamed2002@gmail.com>
2024-11-18 17:13:22 -0600

    tweaked the vite config file

--- client/src/pages/SavedCandidates.tsx
+++ client/src/pages/SavedCandidates.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Candidate from '../interfaces/Candidate.interface';
 
 const SavedCandidates = () => {
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: 'localhost', 
+    port: 5173, 
+  },
   plugins: [react()],
 });
